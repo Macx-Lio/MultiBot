11d05aca55536ca11b94567b1d0dace4a99521c2
Macx-Lio <146089054+Macx-Lio@users.noreply.github.com>
2024-10-09 18:32:13 +0200

    Bugfix
    Bugfix for not cached Items.
    Tooltip did crash the LUA-Script, when the Item was not cached on Server.
    To avoid this, I calculate alternatives from the Playerbot-Infos.

--- MultiBotItem.lua
+++ MultiBotItem.lua
@@ -8,6 +8,10 @@ MultiBot.addItem = function(pFrame, pInfo)
 	local tX = (pFrame.index%8) * 38
 	local tY = math.floor(pFrame.index/8) * -38
 	
+	tName = MultiBot.IF(tName == nil, string.sub(tInfo[4], 3, string.len(tInfo[4]) - 1), tName)
+	tLink = MultiBot.IF(tLink == nil, "|" .. tInfo[2] .. "|" .. tInfo[3] .. "|" .. tInfo[4] .. "|h|r", tLink)
+	tRare = MultiBot.IF(tRare == nil, 0, tRare)
+	
 	local tButton = pFrame.addButton(tName, tX, tY, tIcon, tLink)
 	
 	tButton.item = {}
