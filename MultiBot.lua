eeaa536f1a90fc3863081d3dc2777f7889371357
Macx-Lio <146089054+Macx-Lio@users.noreply.github.com>
2024-10-14 20:19:17 +0200

    ClassIdentification

--- MultiBot.lua
+++ MultiBot.lua
@@ -5,7 +5,6 @@ MultiBotSave = {
 	["MemoryGem1"] = "1;2723.355;-381.056;107.09;Memory-Gem\n|cffffffffThis Memory-Gem will teleport you to 'Kalimdor-Ashenvale'.\nYou need GameMaster-Rights to use this Button.|r\n\n|cffff0000Left-Click to store or teleport to the Location|r\n|cff999999(Execution-Order: Target)|r\n\n|cffff0000Right-Click to forget the Location|r\n|cff999999(Execution-Order: Target)|r;1",
 	["MemoryGem2"] = "1;3215.35;184.837;6.349;Memory-Gem\n|cffffffffThis Memory-Gem will teleport you to 'Kalimdor-Ashenvale'.\nYou need GameMaster-Rights to use this Button.|r\n\n|cffff0000Left-Click to store or teleport to the Location|r\n|cff999999(Execution-Order: Target)|r\n\n|cffff0000Right-Click to forget the Location|r\n|cff999999(Execution-Order: Target)|r;1",
 	["MemoryGem3"] = "0;256.142;-2150.116;118.671;Memory-Gem\n|cffffffffThis Memory-Gem will teleport you to 'Easter-'.\nYou need GameMaster-Rights to use this Button.|r\n\n|cffff0000Left-Click to store or teleport to the Location|r\n|cff999999(Execution-Order: Target)|r\n\n|cffff0000Right-Click to forget the Location|r\n|cff999999(Execution-Order: Target)|r;1",
-	["MultiBarPoint"] = "-262, 144",
 	["ItemusPoint"] = "-860, -145",
 	["SpellbookPoint"] = "-566, 170",
 }
